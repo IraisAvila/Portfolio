46057795e574c28ea8cf22de103bef3f66a8a75b
Irais Avila <iraisavila@Iraiss-Air.attlocal.net>
2022-11-09 22:21:02 -0600

    nav bar/img for logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,61 @@
 //Navbar code and links//
-import React from "react";
-import { FaCode } from "react-icons/fa";
 
-const Navbar = ({ toggle }) => {
+import { useState, useEffect } from "react";
+import {Navbar, Container} from "react-bootstrap"
+import ialogo from '../img/ialogo';
+
+
+export const Navbar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] =useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  },[])
+
+  const onUpdatedActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
   return (
-    <>
-      <Nav>
-        <NavLink to="/">
-          <h1>
-            <FaCode size={30} />
-          </h1>
-        </NavLink>
-        <Bars onClick={toggle} />
-        <NavMenu>
-          <NavLink to="/Aboutme">About me</NavLink>
-          <NavLink to="/Skills">Skills</NavLink>
-          <NavLink to="/Projects">Projects</NavLink>
-          <NavLink to="/Contact">Contact</NavLink>
-        </NavMenu>
-      </Nav>
-    </>
-  );
-};
-
-export default Navbar;
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+    <Container>
+      <Navbar.Brand href="#home">
+        <img src={ialogo} alt="Logo"/>
+        </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav">
+        <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdatedActiveLink('home')}>Home</Nav.Link>
+          <Nav.Link href="#about"className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdatedActiveLink('about')}>About Me</Nav.Link>
+          <Nav.Link href="#projects"className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdatedActiveLink('projects')}>Projects</Nav.Link>
+          <Nav.Link href="#skills"className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdatedActiveLink('skills')}>Skills</Nav.Link>
+          <Nav.Link href="#links"className={activeLink === 'links' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdatedActiveLink('links')}>Links</Nav.Link>
+        </Nav>
+        {/* <span className="navbar-text">
+          <div className="social-icon">
+            <a href="#"><img src={} alt="" /></a>
+            <a href="#"><img src={} alt="" /></a>
+            <a href="#"><img src={} alt="" /></a>
+            <a href="#"><img src={} alt="" /></a>
+            <a href="#"><img src={} alt="" /></a>
+          </div>
+          <button className="vvd" onClick={() => console.log('connect')}><span> Let's Connect</span></button>
+        </span> */}
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+    
+  )
+}
